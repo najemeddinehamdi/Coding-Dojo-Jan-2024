4f98236709e62d66a9eba842c377ba4465416d90
najemeddinehamdi <hnajemeddine@gmail.com>
2024-01-23 14:20:39 +0100

    first commit

---
+++ Programming Basic/Reflection_assignment22.js
@@ -0,0 +1,18 @@
+if(time="before_10am"){
+    console.log("i want a latte")
+}
+else if(time="between_10am_and4pm"){
+    console.log("i want a hot chocolate")
+}
+else if(time="after_10_pm"){
+    console.log("i want to sleep")
+}
+else if(time="between_4am_and_10pm"){
+    console.log("i want ice cream")
+}
+else if(today="wednesday"){
+    console.log("strawberry")
+}
+else if(today="not_wednesday"){
+    console.log("vanilla")
+}
