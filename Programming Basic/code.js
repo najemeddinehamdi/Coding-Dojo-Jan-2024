20ff5c2546c6b2133c189bc9c272782667077905
najemeddinehamdi <hnajemeddine@gmail.com>
2024-01-23 15:38:35 +0100

    code.js

---
+++ Programming Basic/code.js
@@ -0,0 +1,7 @@
+//This variable "likeCount" is inisialised with value of 0
+var likeCount = 0;
+// This function "increaselikes" inceremnt the variable"likeCount" to 1
+function increaseLikes() {
+    // this operation for increase "likecount" by1.
+    likeCount = likeCount + 1;
+}
