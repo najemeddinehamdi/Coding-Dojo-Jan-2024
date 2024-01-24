c3fe859708de19919d94dc3ca0ea81c56668449a
najemeddinehamdi <hnajemeddine@gmail.com>
2024-01-24 17:05:15 +0100

    var

---
+++ Programming Basic/var1/Hamdi_NajemEddine_JS_CreateVariable.js
@@ -0,0 +1,12 @@
+var minimum_age=10;
+var minimum_height=42;
+function GetonThatRide(kiddo){
+    if(var minimum_age<=10)
+    else(var minimum_height<=42){
+        console.log("Get on that ride, kiddo!")
+    }
+    if(var minimum_age =>10)
+    else(var minimum_height=>42){
+        console.log( "Sorry kiddo. Maybe next year.")
+}
+}
