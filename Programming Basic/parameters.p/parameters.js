2a1142da3c3184014503ee4a8457149f2cfd51c3
najemeddinehamdi <hnajemeddine@gmail.com>
2024-01-25 13:59:49 +0100

    parameteres

---
+++ Programming Basic/parameters.p/parameters.js
@@ -0,0 +1,10 @@
+function greet(name, time) {
+    if(name == "Count Dooku") {
+        console.log("I'm coming for you, Dooku!");
+    } else {
+        console.log("Good " + time + " " + name + "!");
+    }
+
+greet("Anakin", "night");
+greet("Count Dooku", "day");
+}
