63e20cbea0ce040c5f8566e7f2b2cc67c83937b3
najemeddinehamdi <hnajemeddine@gmail.com>
2024-01-26 14:52:25 +0100

    loop

---
+++ Programming Basic/loopThrough.op/loop.js
@@ -0,0 +1,11 @@
+// #1
+var arr1 = [8, 6, 7, 5, 3, 0, 9];
+for(i=0;i<arr.length;i++){
+    console.log(arr[i])
+}
+// #2
+var arr2 = [4, 7, 13, 13, 19, 37, -2];
+var sum=0;
+for(i=0;i<arrlength;i++)
+    sum+=arr[i];
+    console.log(sum)
