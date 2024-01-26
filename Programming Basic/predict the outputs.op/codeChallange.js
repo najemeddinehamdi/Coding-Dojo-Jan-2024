fa80dc5a18d754977faa59c6c7d910351ec4c4d6
najemeddinehamdi <hnajemeddine@gmail.com>
2024-01-26 14:19:00 +0100

    codeChallenge

---
+++ Programming Basic/predict the outputs.op/codeChallange.js
@@ -0,0 +1,29 @@
+//challenge 1:
+var arr = ["Up", "You", "Give", "To", "Going", "Never"];
+for(var i = arr.length-1; i >= 0; i = i - 1) {
+    console.log(arr[i]);
+}
+// the output is [never going to give you up]
+
+
+//challenge 2:
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var sum = 0;
+for(var i = 1; i < arr.length; i = i + 2) {
+    sum = sum + arr[i];
+    console.log(arr[i]);
+    console.log(sum);
+}
+
+// the output is [2,4,6,8,10,2,6,12,20,30]
+
+
+// challenge 3:
+var arr = [8, 2, 0, 6, 12, 4, 3];
+for(var i = 0; i < arr.length; i++){
+    if(arr[i] >= 6) {
+        console.log(true);
+    }
+}
+
+// the output is [true,true,true]
