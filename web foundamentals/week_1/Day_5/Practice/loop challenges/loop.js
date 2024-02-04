4ffd24c38d0e7e30c9e441216605f4eaaf64d536
najemeddinehamdi <hnajemeddine@gmail.com>
2024-02-04 15:36:00 +0100

    loop

---
+++ web foundamentals/week_1/Day_5/Practice/loop challenges/loop.js
@@ -0,0 +1,31 @@
+//print odd 1-20
+for(i=1; i<=20;i++){
+    if(i%2==1)
+    console.log(i)
+}
+
+//Decreasing Multiples of 3
+for ( i = 100; i >= 0; i--) {
+    if (i % 3 === 0) {
+      console.log(i);
+    }
+  }
+
+//Print the sequence
+for ( i = 4; i >= -3.5; i -= 1.5) {
+    console.log(i);
+  }
+
+//Sigma
+sum=0
+for(i=1; i<=100; i++){
+    sum=sum+i
+    console.log(sum)
+}
+
+//Factorial
+sum=1
+for(i=2;i<=12; i++){
+    sum=sum*i
+    console.log(sum)
+}
