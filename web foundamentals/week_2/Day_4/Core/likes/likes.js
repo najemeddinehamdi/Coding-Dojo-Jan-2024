3056297d803879c8372b12fb43cc8ae210af4ca9
najemeddinehamdi <hnajemeddine@gmail.com>
2024-02-08 13:14:53 +0100

    likes

---
+++ web foundamentals/week_2/Day_4/Core/likes/likes.js
@@ -0,0 +1,7 @@
+var count = 1;
+    var span= document.querySelector("span");
+function add1(){
+    count++;
+    span.innerHTML=count;
+    
+}
---
+++ web foundamentals/week_2/Day_4/Core/part-2/like.js
@@ -0,0 +1,18 @@
+var count = 1;
+    var span= document.querySelector(".s1");
+function add1(){
+    count++;
+    span.innerHTML=count;
+}
+var count1 = 1;
+    var span1= document.querySelector(".s2 ");
+function add2(){
+    count1++;
+    span1.innerHTML=count1;
+}
+var count2 = 1;
+    var span2= document.querySelector(".s3");
+function add3(){
+    count2++;
+    span2.innerHTML=count2;
+}
