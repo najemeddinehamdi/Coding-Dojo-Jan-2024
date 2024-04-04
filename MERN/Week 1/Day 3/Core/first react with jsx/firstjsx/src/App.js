dff8462bbdafbe4bfee08ea52f1a8330b23fb4a3
najemeddinehamdi <hnajemeddine@gmail.com>
2024-04-04 20:16:15 +0100

    first react with jsx

---
+++ MERN/Week 1/Day 3/Core/first react with jsx/firstjsx/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Dojo!</h1>
+        <h2>Things i need to do:</h2>
+        <ul>
+          <li>learn react</li>
+          <li>climb Mt.Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
