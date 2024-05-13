a44b780d5909329b44d8bb124d568a90386fa043
najemeddinehamdi <hnajemeddine@gmail.com>
2024-05-13 17:17:11 +0100

    first

---
+++ Java/Week 1/Day 1/Practice/first java program/First.java
@@ -0,0 +1,7 @@
+public class First{
+    public static void main(String[]args){
+        System.out.println("My name is najem eddine hamdi");
+        System.out.println("I am 23 years old");
+        System.out.println("My home town is tunisia");
+    }
+}
